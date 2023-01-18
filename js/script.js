415957a4591ef5c7fea9296de30cabe7aaf4cb75
Michal Maciejewski <michao931@gmail.com>
2023-01-18 20:25:59 +0100

    adding new task

--- js/script.js
+++ js/script.js
@@ -22,10 +22,33 @@
       }
   
       document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
-    const init = () => {};
-    render();
+    };
+
+    const addNewTask = () => {
+      tasks.push({
+        content: newTaskContent,
+      });
+
+      render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
   
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if(newTaskContent === "") {
+          return;
+        }
+  
+        addNewTask(newTaskContent);
+      };
+    
+    const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onFormSubmit );
+  };
     init();
   }
   
