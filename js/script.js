ddf66e5612132abab108dd782fdd032fd946e346
Michal Maciejewski <michao931@gmail.com>
2023-01-18 20:51:43 +0100

    toggle task done

--- js/script.js
+++ js/script.js
@@ -23,12 +23,20 @@
       render();
     }
 
+    const toggleTaskDone = (taskIndex) => {
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+      render();
+    }
+
     const render = () => {
       let htmlString = "";
   
       for (const task of tasks) {
           htmlString += `
-          <li>
+          <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+          >
+          
+          <button class="js-done">zrobione?</button>
           <button class="js-remove">usuń</button>
           ${task.content}
           </li>
@@ -42,6 +50,14 @@
       removeButtons.forEach((removeButton, index) => {
         removeButton.addEventListener("click", () =>{
          removeTask(index);
+        });
+      });
+
+      const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+      toggleDoneButtons.forEach((toggleDoneButton, index) => {
+        toggleDoneButton.addEventListener("click", () =>{
+         toggleTaskDone(index);
         });
       });
     };
