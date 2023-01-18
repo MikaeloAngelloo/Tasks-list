cc7b6f99dbb983b722644c7f5926de53b81f79ff
Michal Maciejewski <michao931@gmail.com>
2023-01-18 20:43:17 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,27 +10,43 @@
       },
     ];
   
-    const render = () => {
-      let htmlString = "";
-  
-      for (const task of tasks) {
-          htmlString += `
-          <li>
-          ${task.content}
-          </li>
-          `;
-      }
-  
-      document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
-    const addNewTask = () => {
+    const addNewTask = (newTaskContent) => {
       tasks.push({
         content: newTaskContent,
       });
 
       render();
     };
+
+    const removeTask = (taskIndex) => {
+      tasks.splice(taskIndex, 1)
+      render();
+    }
+
+    const render = () => {
+      let htmlString = "";
+  
+      for (const task of tasks) {
+          htmlString += `
+          <li>
+          <button class="js-remove">usuń</button>
+          ${task.content}
+          </li>
+          `;
+      }
+  
+      document.querySelector(".js-tasks").innerHTML = htmlString;
+
+      const removeButtons = document.querySelectorAll(".js-remove");
+
+      removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () =>{
+         removeTask(index);
+        });
+      });
+    };
+
+    
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -41,7 +57,7 @@
         }
   
         addNewTask(newTaskContent);
-      };
+      }; 
     
     const init = () => {
     render();
